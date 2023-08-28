6c7d1778841ce2f80a89cfccd0ef03bab2e339ea
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-28 20:23:03 +0300

    complete on word_frequency.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -6,4 +6,4 @@ def remove_duplicates(sequence):
 
     return container
 
-print(remove_duplicates([2, 3, 2, 4, 5, 3, 6, 7, 5])) 
+print(remove_duplicates([2, 3, 2, 4, 5, 3, 6, 7, 5]))  # [2, 3, 4, 5, 6, 7]
--- word_frequency.py
+++ word_frequency.py
@@ -1,2 +1,11 @@
+import re
+from collections import Counter
+
 def word_frequency(sentence):
-    pass
+    words = re.findall(r'\b\w+\b', sentence.lower()) # use regex to get a list of sentence substrings
+    word_frequency = Counter(words)  # create a Counter object to auto-count word frequencies from the words list
+    return dict(word_frequency)  # convert the Counter object to a regular dictionary
+
+sentence = "This is a test sentence? This is a test sentence."
+
+print(word_frequency(sentence))  # {'this': 2, 'is': 2, 'a': 2, 'test': 2, 'sentence': 2}
