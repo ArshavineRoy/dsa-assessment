bbcfa56bff67dee42145a53781b4914f11f8ea09
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-28 20:54:14 +0300

    refactor is_balanced.py to use deque for stack

--- is_balanced.py
+++ is_balanced.py
@@ -1,20 +1,24 @@
-def is_balanced(expression):
-    brackets = {
-        ")":"(",
-        "}": "{",
-        "]": "["
-        }
-    
-    stack =[]
-    for char in expression:
-        if char in '({[':
-            stack.append(char)
-        elif char in ')}]':
-            if not stack or stack.pop() != brackets[char]:
-                return False
-    return not stack 
-             
-expression1 = '([]{})'
-expression2 = '([))'
+from collections import deque
 
-print(is_balanced(expression2))
+def is_balanced(expression):
+    stack = deque()  # Create an empty stack
+    brackets = {
+        "(": ")",    # Map opening brackets to their corresponding closing brackets
+        "{": "}",
+        "[": "]"
+    }
+
+    for char in expression:
+        if char in brackets.keys():  # If the character is an opening bracket, ({[ 
+            stack.append(char)       # Push it onto the stack
+            
+        elif char in brackets.values():  # If the character is a closing bracket, )}]
+            if not stack or brackets[stack.pop()] != char:  # Check if the stack is empty or mismatched
+                return False
+    
+    return not stack  # If the stack is empty, the expression is balanced
+
+expression1 = "([]{})"
+expression2 = "([)]"
+print(is_balanced(expression1))  # True
+print(is_balanced(expression2))  # False
